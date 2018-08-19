24c266923572585c66f71d99c88f0d9e79fba43e
Adam Ostrožlík <adamostrozlik@yandex.com>
2018-08-19 22:11:44 +0200

    Spring MVC 1/2

---
+++ springmvc/src/test/java/SpringMvcTest.java
@@ -0,0 +1,21 @@
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.web.context.WebApplicationContext;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration
+@WebAppConfiguration
+public class SpringMvcTest {
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    @Test
+    public void testHomeController() {
+
+    }
+}
