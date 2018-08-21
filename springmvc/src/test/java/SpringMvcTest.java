03320312a11fc61953070b6583e625c06c361994
Adam Ostrožlík <adamostrozlik@yandex.com>
2018-08-21 09:14:41 +0200

    Spring MVC 2/2

--- springmvc/src/test/java/SpringMvcTest.java
+++ springmvc/src/test/java/SpringMvcTest.java
@@ -1,14 +1,21 @@
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration
-@WebAppConfiguration
+@Configuration
+@EnableWebMvc
+@ComponentScan("controller")
 public class SpringMvcTest {
 
     @Autowired
@@ -16,6 +23,16 @@ public class SpringMvcTest {
 
     @Test
     public void testHomeController() {
+    }
 
+    @Bean
+    public ViewResolver viewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+
+        viewResolver.setViewClass(JstlView.class);
+        viewResolver.setPrefix("/WEB-INF/views/");
+        viewResolver.setSuffix(".jsp");
+
+        return viewResolver;
     }
 }
